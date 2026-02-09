01181c18850596ce905b5c9b94ed56007835f13f
Jcook006 <jcook@tailoredagents.com>
2026-02-09 18:07:11 -0500

    Nav: move Sales HQ out of quick

--- apps/site/src/app/team/page.tsx
+++ apps/site/src/app/team/page.tsx
@@ -737,7 +737,7 @@ export default async function TeamPage({
 
   const allowedTabs = resolvedTabs.filter((item) => isAllowed(item.requires));
   const tabMap = new Map(allowedTabs.map((item) => [item.id, item]));
-  const quickIds = ["inbox", "chat", "contacts", "calendar", "sales-hq"];
+  const quickIds = ["inbox", "chat", "contacts", "calendar"];
   const quickIdSet = new Set(quickIds);
   const groups: TeamNavGroup[] = tabGroups
     .filter((group) => group.id !== "account")
